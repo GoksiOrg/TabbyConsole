955683ac787d92c8e2c08a8d0752b7c9a088bb89
Vukašin Lekić <goksi1312@gmail.com>
2023-02-08 16:36:25 +0100

    sad

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+
+    plugins: [
+        laravel({
+            input: [
+                'resources/js/index.tsx',
+                'resources/sass/app.scss'
+            ],
+            refresh: true,
+        }),
+        react({
+            fastRefresh: false
+        })
+    ],
+});
