7a6075ec9a345ca3a2bc6e6afc9634aafe537a73
Vukašin Lekić <goksi1312@gmail.com>
2023-02-11 00:06:15 +0100

    Adding favicons

--- resources/views/wrapper.blade.php
+++ resources/views/wrapper.blade.php
@@ -7,10 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="robots" content="noindex">
+    <link rel="icon" type="image/png" href={{url("/img/favicon-32x32.png")}} sizes="32x32">
+    <link rel="icon" type="image/png" href={{url("/img/favicon-16x16.png")}} sizes="16x16">
     {{--    todo: favicons--}}
     @section("user-info")
-        @if(!is_null(Auth::user()))
-            window.User = {!! json_encode(Auth::user()->toObject()) !!}
+        @if(\Illuminate\Support\Facades\Auth::check())
+            <script>
+                window.User = {!! json_encode(Auth::user()->toObject()) !!};
+            </script>
         @endif
     @show
     @vite(['resources/sass/app.scss', 'resources/js/index.tsx'])
@@ -18,6 +22,7 @@
     <style>
         body {
             background-color: #1a202c;
+            color: white;
         }
     </style>
     <title>{{ config('app.name', 'RemoteConsole') }}</title>
