1777a917efa299c30770ae062cbe6afc115e1eed
Vukašin Lekić <goksi1312@gmail.com>
2023-02-21 00:36:45 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ServersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('servers')->group(function () {
+    Route::get('/', [ServersController::class, 'index']);
+    Route::post('/', [ServersController::class, 'store']);
 });
