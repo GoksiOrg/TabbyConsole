b89f7b0e230a72c8a99a79657b565d21b7105b2a
Vukašin Lekić <goksi1312@gmail.com>
2023-03-05 11:48:35 +0100

    Update ResourceController.php

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -5,13 +5,11 @@ namespace App\Http\Controllers;
 use App\Exceptions\ServerConnectionException;
 use App\Models\Server;
 use App\Repository\ControlRepository;
-use Carbon\Carbon;
-use Illuminate\Cache\Repository;
 use Illuminate\Http\JsonResponse;
 
 class ResourceController extends Controller
 {
-    public function __construct(private readonly ControlRepository $repository, private readonly Repository $cache)
+    public function __construct(private readonly ControlRepository $repository)
     {
     }
 
@@ -20,10 +18,7 @@ class ResourceController extends Controller
      */
     public function index(Server $server): JsonResponse
     {
-        $cacheKey = "resourceCache:$server->id";
-        $resources = $this->cache->remember($cacheKey, Carbon::now()->addSeconds(20), function () use ($server) {
-            return $this->repository->setServer($server)->getResources();
-        });
+        $resources = $this->repository->setServer($server)->getResources();
 
         return response()->json($resources);
     }
