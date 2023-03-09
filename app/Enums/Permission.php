999847ea22373756c7609dda2a79017101e62128
Vukašin Lekić <goksi1312@gmail.com>
2023-03-09 17:27:34 +0100

    refactor

--- app/Enums/Permission.php
+++ app/Enums/Permission.php
@@ -5,7 +5,8 @@ namespace App\Enums;
 enum Permission: int
 {
     case ADMINISTRATOR = 1 << 0;
-    case CONSOLE = 1 << 1;
-    case VIEW_FILES = 1 << 2;
-    case EDIT_FILES = 1 << 3;
+    case WEBSOCKET_CONNECT = 1 << 1;
+    case WEBSOCKET_SEND_COMMAND = 1 << 2;
+    case VIEW_FILES = 1 << 3;
+    case EDIT_FILES = 1 << 4;
 }
