48d24286b44e37c3c97f98ca25ab2b78b366daaa
Vukašin Lekić <goksi1312@gmail.com>
2023-03-31 02:17:50 +0200

    :dizzy_face:

--- resources/js/helpers/api/local/storeServer.ts
+++ resources/js/helpers/api/local/storeServer.ts
@@ -8,13 +8,14 @@ interface StoreServerRequest {
     scheme: "http" | "https";
 }
 
-interface SecretResponse {
+export interface SecretResponse {
+    id: number;
     secret: string;
 }
 
 export default function storeServer(server: StoreServerRequest): Promise<SecretResponse> {
     return new Promise((resolve, reject) => {
-        http.post("/servers", server)
+        http.post("/api/servers", server)
             .then(result => resolve(result.data))
             .catch(reject);
     });
