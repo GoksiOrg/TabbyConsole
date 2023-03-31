809363b570d27fb69bec763a4825c331d48d8b4a
Vukašin Lekić <goksi1312@gmail.com>
2023-03-31 02:18:05 +0200

    Refactor modal

---
+++ resources/js/components/server/TabbyModal.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function TabbyModal(props: { title: string; children: React.ReactNode }) {
+    return (
+        <div className="modal fade" tabIndex={-1} id="addServerModal">
+            <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">{props.title}</h5>
+                    </div>
+                    <div className="modal-body">{props.children}</div>
+                    <div className="modal-footer">
+                        <button className="btn btn-secondary btn-block" data-bs-dismiss="modal">
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
