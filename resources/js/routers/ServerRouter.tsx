2232ea6ba837135adb880a8c4377f3bdc210f704
Vukašin Lekić <goksi1312@gmail.com>
2023-04-04 15:17:40 +0200

    Update ServerRouter.tsx

--- resources/js/routers/ServerRouter.tsx
+++ resources/js/routers/ServerRouter.tsx
@@ -1,3 +1,9 @@
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { ErrorStatus } from "../components/Error";
+
 export default function ServerRouter() {
+    const params = useParams<"id">();
+    const [error, setError] = useState<ErrorStatus>();
     return <h1>smth</h1>;
 }
