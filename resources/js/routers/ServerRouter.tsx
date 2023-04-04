489e791f7873531a2f3cee3f0fa6bfe249914ee6
Vukašin Lekić <goksi1312@gmail.com>
2023-04-05 00:53:58 +0200

    Routing

--- resources/js/routers/ServerRouter.tsx
+++ resources/js/routers/ServerRouter.tsx
@@ -1,9 +1,48 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import { ErrorStatus } from "../components/Error";
+import { ServerStore } from "../states/server";
+import { useEffect } from "react";
+import { AxiosError } from "axios";
+import Error from "../components/Error";
+import Loading from "../components/Loading";
+import NavBar from "../components/global/NavBar";
 
 export default function ServerRouter() {
     const params = useParams<"id">();
     const [error, setError] = useState<ErrorStatus>();
-    return <h1>smth</h1>;
+
+    const getServer = ServerStore.useStoreActions(actions => actions.getServer);
+    const clearServerState = ServerStore.useStoreActions(actions => actions.clearState);
+    const name = ServerStore.useStoreState(state => state.server?.name);
+
+    useEffect(() => {
+        if (params.id === undefined) {
+            return;
+        }
+
+        getServer(parseInt(params.id)).catch((err: AxiosError) => {
+            console.log(err);
+            setError({ statusCode: err.response.status, message: err.response.statusText });
+        });
+
+        return () => {
+            clearServerState();
+        };
+    }, [params.id]);
+
+    return (
+        <>
+            <NavBar />
+            {!name ? (
+                error === undefined ? (
+                    <Loading />
+                ) : (
+                    <Error error={error} />
+                )
+            ) : (
+                <h1>h11 {name}</h1>
+            )}
+        </>
+    );
 }
--- resources/js/states/server/index.ts
+++ resources/js/states/server/index.ts
@@ -1,11 +1,30 @@
 import { createContextStore } from "easy-peasy";
 import { type Server } from "../../helpers/api/local/getServersPaginator";
 import { Action } from "easy-peasy";
-
+import { action } from "easy-peasy";
+import { Thunk } from "easy-peasy";
+import { thunk } from "easy-peasy";
+import getServer from "../../helpers/api/local/getServer";
+/*TODO: permissions*/
 interface ServerData {
     server?: Server;
 
     setServer: Action<ServerData, Server>;
+
+    clearState: Action<ServerData>;
+
+    getServer: Thunk<ServerData, number, any, ServerData, Promise<void>>;
 }
 
-export const ServerStore = createContextStore<ServerData>({});
+const store: ServerData = {
+    server: undefined,
+    setServer: action((state, payload) => {
+        state.server = payload;
+    }),
+    clearState: action(state => (state.server = undefined)),
+    getServer: thunk(async (actions, payload) => {
+        const server = await getServer(payload);
+        actions.setServer(server);
+    }),
+};
+export const ServerStore = createContextStore<ServerData>(store);
