ecba615563954496720624008adfa854e22848fe
Vukašin Lekić <goksi1312@gmail.com>
2023-04-11 14:32:02 +0200

    Update SubNavBar.tsx

--- resources/js/components/server/SubNavBar.tsx
+++ resources/js/components/server/SubNavBar.tsx
@@ -1,9 +1,12 @@
+import { ServerStore } from "../../states/server";
+
 export default function SubNavBar() {
+    const permissions = ServerStore.useStoreState(state => state.server.permissions);
     return (
         <nav
             className="navbar mb-3"
             style={{ backgroundColor: "#1a202c" }}
-            aria-label="Subnavigation"
+            aria-label="SubNavigation"
         >
             sadasda adasd asda
         </nav>
